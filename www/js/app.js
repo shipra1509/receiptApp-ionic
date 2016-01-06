4c986ce3fd2b54a5a466564c4203f4774f2cf60a
Shipra Sinha <shipra.juet@gmail.com>
2016-01-06 20:41:05 +0530

    Create static pages for 'create', 'display', 'my receipts'

--- www/js/app.js
+++ www/js/app.js
@@ -32,42 +32,34 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('app.create', {
+    url: '/create',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/create.html'
       }
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
+  .state('app.myReceipts', {
+      url: '/myReceipts',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+          templateUrl: 'templates/myReceipts.html'
         }
       }
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+
+  .state('app.display', {
+      url: '/display',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/display.html'
+        }
       }
-    }
-  });
+    })
+ 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/myReceipts');
 });
