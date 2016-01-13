08e63f718bf3fccf54328fb1fbf3249dc9cd33e4
Shipra Sinha <shipra.juet@gmail.com>
2016-01-13 22:16:56 +0530

    Add local storage using ng-storage

--- www/js/app.js
+++ www/js/app.js
@@ -4,18 +4,22 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngOpenFB', 'ngStorage'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, ngFB) {
+  ngFB.init({appId: '945230595567558'});
+
   $ionicPlatform.ready(function() {
+
+
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  if (window.cordova && window.cordova.plugins.Keyboard) {
+    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
+  }
+  if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
@@ -25,7 +29,14 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('app', {
+  .state('/', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'login.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
@@ -42,24 +53,24 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   })
 
   .state('app.myReceipts', {
-      url: '/myReceipts',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/myReceipts.html'
-        }
+    url: '/myReceipts',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/myReceipts.html'
       }
-    })
-
+    }
+  })
 
   .state('app.display', {
-      url: '/display',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/display.html'
-        }
+    url: '/display',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/display.html'
       }
-    })
- 
+    }
+  })
+
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/myReceipts');
+  $urlRouterProvider.otherwise('/index');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngOpenFB'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, ngFB, $state, $localStorage, $sessionStorage) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -10,7 +10,6 @@ angular.module('starter.controllers', [])
   //});
 
   // Form data for the login modal
-  $scope.loginData = {};
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -31,6 +30,7 @@ angular.module('starter.controllers', [])
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
+
     console.log('Doing login', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
@@ -39,18 +39,34 @@ angular.module('starter.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
-})
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+  $scope.fbLogin = function () {
+    ngFB.login({scope: 'email'}).then(
+      function (response) {
+        if (response.status === 'connected') {
+          console.log('Facebook login succeeded');
+          $scope.closeLogin();
+        } else {
+          alert('Facebook login failed');
+        }
+      }
+    );
+  };
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+  $scope.$storage = $localStorage.$default({
+    receipts: []
 });
+
+
+  $scope.newReceipt = {};
+  $scope.receipts = $scope.$storage.receipts;
+
+  $scope.createReceipt = function() {
+    $scope.receipts.push($scope.newReceipt);
+    $state.go('app.myReceipts');
+  };
+})
+
+
+
+
